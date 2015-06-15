88eb65765bf6cea22813df61774bdaba7c6ebfc0
Preben Hermans <preben.hermans@staff.telenet.be>
2015-06-15 23:35:23 +0200

    favicon changed

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -19,13 +19,13 @@ angular.module('matukioApp')
 angular.module('matukioApp')
     .controller('EventCtrl', function ($scope) {
         $scope.events = [
-            {eventid:'1', eventtitle:'Event 1',imageurl:'images/dummy-project.jpg', category:'Reading Group'},
-            {eventid:'2', eventtitle:'Event 2',imageurl:'images/dummy-project.jpg', category:'Reading Group'},
-            {eventid:'3', eventtitle:'Event 3',imageurl:'images/dummy-project.jpg', category:'Reading Group'},
-            {eventid:'4', eventtitle:'Event 4',imageurl:'images/dummy-project.jpg', category:'Reading Group'},
-            {eventid:'5', eventtitle:'Event 5',imageurl:'images/dummy-project.jpg', category:'Reading Group'},
-            {eventid:'6', eventtitle:'Event 6',imageurl:'images/dummy-project.jpg', category:'Reading Group'},
-            {eventid:'7', eventtitle:'Event 7',imageurl:'images/dummy-project.jpg', category:'Reading Group'},
+            {eventid:'1', eventtitle:'Event 1',imageurl:'<img src="images/dummy-project.jpg" alt="">', category:'Reading Group'},
+            {eventid:'2', eventtitle:'Event 2',imageurl:'<img src="images/dummy-project.jpg" alt="">', category:'Reading Group'},
+            {eventid:'3', eventtitle:'Event 3',imageurl:'<img src="images/dummy-project.jpg" alt="">', category:'Reading Group'},
+            {eventid:'4', eventtitle:'Event 4',imageurl:'<img src="images/dummy-project.jpg" alt="">', category:'Reading Group'},
+            {eventid:'5', eventtitle:'Event 5',imageurl:'<img src="images/dummy-project.jpg" alt="">', category:'Reading Group'},
+            {eventid:'6', eventtitle:'Event 6',imageurl:'<img src="images/dummy-project.jpg" alt="">', category:'Reading Group'},
+            {eventid:'7', eventtitle:'Event 7',imageurl:'<img src="images/dummy-project.jpg" alt="">', category:'Reading Group'},
 
         ];
     });
