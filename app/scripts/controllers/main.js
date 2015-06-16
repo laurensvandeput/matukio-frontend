7598bff6b3b67e051a56c322a7ad9235fddfc83d
Preben Hermans <preben.hermans@staff.telenet.be>
2015-06-16 14:41:19 +0200

    styling

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -33,20 +33,12 @@ angular.module('matukioApp')
         eventsService.getEventsOverview()
             .success(function(data) {
                 $scope.events = data;
-        });
-        /*
-        $scope.events = [
-            {eventid:'1', eventtitle:'Event 1',imageurl:'<img src="images/dummy-project.jpg" alt="">', category:'Reading Group'},
-            {eventid:'2', eventtitle:'Event 2',imageurl:'<img src="images/dummy-project.jpg" alt="">', category:'Reading Group'},
-            {eventid:'3', eventtitle:'Event 3',imageurl:'<img src="images/dummy-project.jpg" alt="">', category:'Reading Group'},
-            {eventid:'4', eventtitle:'Event 4',imageurl:'<img src="images/dummy-project.jpg" alt="">', category:'Reading Group'},
-            {eventid:'5', eventtitle:'Event 5',imageurl:'<img src="images/dummy-project.jpg" alt="">', category:'Reading Group'},
-            {eventid:'6', eventtitle:'Event 6',imageurl:'<img src="images/dummy-project.jpg" alt="">', category:'Reading Group'},
-            {eventid:'7', eventtitle:'Event 7',imageurl:'<img src="images/dummy-project.jpg" alt="">', category:'Reading Group'},
 
-        ];
-        */
+        });
+
     }]);
+
+
 
 
 
