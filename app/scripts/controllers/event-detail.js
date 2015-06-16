ac7b026b3b90a1220292a8633a0526b1366e0ecc
Preben Hermans <preben.hermans@staff.telenet.be>
2015-06-16 11:35:48 +0200

    event detail updated

--- app/scripts/controllers/event-detail.js
+++ app/scripts/controllers/event-detail.js
@@ -2,12 +2,25 @@
 
 /**
  * @ngdoc function
- * @name matukioApp.controller:AboutCtrl
+ * @name matukioApp.controller:eventDetailCtrl
  * @description
- * # AboutCtrl
+ * # EventDetailControler
  * Controller of the matukioApp
  */
+
 angular.module('matukioApp')
-  .controller('EventDetailCtrl', function ($scope, $routeParams) {
-        $scope.eventid = $routeParams.eventId;
-  });
+    .controller('eventDetailCtrl', ['$scope','$http', function($scope, $http,$routeParams){
+        $http.get('scripts/data.json').success (function(data){
+
+            console.log('json call win!');
+            $scope.eventid = $routeParams.eventId;
+
+            angular.forEach(data.events, function(event) {
+
+                if($scope.eventid === data.eventid){
+                    $scope.eventDetails = event;
+                }
+            });
+        });
+    }]
+ );
